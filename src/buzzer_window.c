b086a820651ab1995dd6544b3e38db086ed29a8c
Nick Vella <nick@nxk.me>
2015-08-29 22:16:02 +1000

    timer pausing

--- src/buzzer_window.c
+++ src/buzzer_window.c
@@ -5,6 +5,7 @@
 #include "buzzer_window.h"
 
 int buzzer_window_time;
+bool buzzer_window_paused;
 
 void buzzer_window_update_time() {
   // Create countdown string
@@ -24,6 +25,8 @@ void buzzer_window_update_time() {
 }
 
 void buzzer_window_timer_handler(void *context) {
+  if(buzzer_window_paused) return; // Don't do anything if the timer is paused
+
   buzzer_window_time--;
 
   if(buzzer_window_time < 0) {
@@ -40,7 +43,20 @@ void buzzer_window_timer_handler(void *context) {
 }
 
 void buzzer_window_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  // Pause the timer
+  // (Un)pause the timer
+  buzzer_window_paused = !buzzer_window_paused;
+
+  if(buzzer_window_paused) {
+    // Set paused text layer
+    text_layer_set_text(buzzer_window_text_layer_paused, "Paused");
+  } else {
+    // Blank paused layer
+    text_layer_set_text(buzzer_window_text_layer_paused, "");
+    // Reset timer
+    app_timer_register(1000, buzzer_window_timer_handler, NULL);
+  }
+
+  vibes_short_pulse();
 }
 
 void buzzer_window_click_config_provider(void *context) {
@@ -52,8 +68,9 @@ void buzzer_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // Set buzzer time
+  // Set buzzer time and paused status
   buzzer_window_time = main_window_buzz_time_mins * 60 - 1;
+  buzzer_window_paused = false;
 
   // Set up time text layer "XX mins"
   buzzer_window_text_layer_time = text_layer_create(GRect(0, bounds.size.h / 2 - 42 / 2 - 10, bounds.size.w, 42));
@@ -61,6 +78,12 @@ void buzzer_window_load(Window *window) {
   text_layer_set_text_alignment(buzzer_window_text_layer_time, GTextAlignmentCenter);
   text_layer_set_font(buzzer_window_text_layer_time, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(buzzer_window_text_layer_time));
+
+  // Set up paused text layer
+  buzzer_window_text_layer_paused = text_layer_create(GRect(0, bounds.size.h / 4 - 20 / 2 - 11, bounds.size.w, 22));
+  text_layer_set_text_alignment(buzzer_window_text_layer_paused, GTextAlignmentCenter);
+  text_layer_set_font(buzzer_window_text_layer_paused, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  layer_add_child(window_layer, text_layer_get_layer(buzzer_window_text_layer_paused));
 
   // Set the timer
   app_timer_register(1000, buzzer_window_timer_handler, NULL);
--- src/buzzer_window.h
+++ src/buzzer_window.h
@@ -2,6 +2,7 @@
 
 Window *buzzer_window;
 TextLayer *buzzer_window_text_layer_time;
+TextLayer *buzzer_window_text_layer_paused;
 
 void buzzer_window_click_config_provider(void *context);
 
